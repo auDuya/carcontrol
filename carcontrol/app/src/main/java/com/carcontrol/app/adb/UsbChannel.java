4f168d99d6f02116cb693eb04ceb9477dcb77b66
auDuya <uduya@foxmail.com>
2025-09-23 17:00:47 +0800

    Update UsbChannel.java

--- carcontrol/app/src/main/java/com/carcontrol/app/adb/UsbChannel.java
+++ carcontrol/app/src/main/java/com/carcontrol/app/adb/UsbChannel.java
@@ -31,7 +31,7 @@ public class UsbChannel implements AdbChannel {
   public UsbChannel(UsbDevice usbDevice) throws IOException {
     // 连接USB设备
     if (AppData.usbManager == null) throw new IOException("not have usbManager");
-   
+    usbConnection = AppData.usbManager.openDevice(usbDevice);
     if (usbConnection == null) return;
     // 查找ADB的接口
     for (int i = 0; i < usbDevice.getInterfaceCount(); i++) {

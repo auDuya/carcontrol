642a9fd5715cf6f66b8241b4ae5482e52c654270
auDuya <uduya@foxmail.com>
2025-09-23 13:06:11 +0800

    Update FullActivity.java

--- carcontrol/app/src/main/java/com/carcontrol/app/client/view/FullActivity.java
+++ carcontrol/app/src/main/java/com/carcontrol/app/client/view/FullActivity.java
@@ -114,7 +114,6 @@ public class FullActivity extends Activity {
     if (!clientView.lightState) fullActivity.buttonLightOff.setImageResource(R.drawable.lightbulb);
     fullActivity.buttonLightOff.setOnClickListener(v -> {
       if (clientView.lightState) {
-        clientView.onClose.run();
         clientView.controlPacket.sendLightEvent(Display.STATE_UNKNOWN);
         fullActivity.buttonLightOff.setImageResource(R.drawable.lightbulb);
         clientView.lightState = false;
